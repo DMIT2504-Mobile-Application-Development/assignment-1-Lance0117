0e55446f5e81c2ab03026fa1e88574f9e6c5bb27
Lance0117 <fyang16@nait.ca>
2025-05-23 04:23:55 -0600

    Update weather_api.dart
    Part 1 done.

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+/// Returns JSON from a provided endpoint
+///
+/// Will throw a [FormatException] if the [endpoint] is not a well-formed URI.
+Future<dynamic> getWeatherForCity(String city) async {
+  const weatherApiKey = "8e1a75480b4c86674740069a268fc4b0";
+  final currentWeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${weatherApiKey}";
+
+  final url = Uri.parse(currentWeatherEndpoint);
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    print('There was a problem with the request: status ${response.statusCode} received');
+    print('Response body: ${response.body}');
+    return null;
+  }
+}
