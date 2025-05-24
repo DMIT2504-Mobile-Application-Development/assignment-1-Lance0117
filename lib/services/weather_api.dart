c6086c5f80ece66ddfbe5a7e9450a591f946d8da
Lance0117 <fyang16@nait.ca>
2025-05-24 02:50:30 -0600

    Update weather_api.dart
    updated named parameter

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 /// Returns JSON from a provided endpoint
 ///
 /// Will throw a [FormatException] if the [endpoint] is not a well-formed URI.
-Future<dynamic> getWeatherForCity(String city) async {
+Future<dynamic> getWeatherForCity({required String city}) async {
   const weatherApiKey = "8e1a75480b4c86674740069a268fc4b0";
   final currentWeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=$city&appid=$weatherApiKey";
 
