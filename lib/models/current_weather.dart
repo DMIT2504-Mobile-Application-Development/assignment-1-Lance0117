438bb5c38a95e26b1fdf9454c11e1c32edc4e5cd
Lance0117 <fyang16@nait.ca>
2025-05-24 03:12:13 -0600

    Update current_weather.dart
    print successfully.

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -1,5 +1,5 @@
 class CurrentWeather {
- //region private members
+  //region private members
   late String _city;
   late String _description;
   late double _currentTemp;
@@ -7,13 +7,31 @@ class CurrentWeather {
   late DateTime _sunrise;
   late DateTime _sunset;
 
-  CurrentWeather(String city, String description, double currentTemp, DateTime currentTime, DateTime sunrise, DateTime sunset) {
+  CurrentWeather(String city, String description, double currentTemp,
+      DateTime currentTime, DateTime sunrise, DateTime sunset) {
     this.city = city;
     this.description = description;
     this.currentTemp = currentTemp;
     this.currentTime = currentTime;
-    this.sunrise = sunrise;
+
+    if (!(sunrise.year == currentTime.year &&
+        sunrise.month == currentTime.month &&
+        sunrise.day == currentTime.day)) {
+      throw Exception('Sunrise must be on the same day as current time');
+    } else if (sunrise.isAfter(sunset)) {
+      throw Exception('Sunrise cannot be after sunset');
+    }
+
+    if (!(sunset.year == currentTime.year &&
+        sunset.month == currentTime.month &&
+        sunset.day == currentTime.day)) {
+      throw Exception('Sunset must be on the same day as current time');
+    } else if (sunset.isBefore(sunrise)) {
+      throw Exception('Sunset cannot be before sunrise');
+    }
+
     this.sunset = sunset;
+    this.sunrise = sunrise;
   }
 
   // Define properties
@@ -27,7 +45,6 @@ class CurrentWeather {
     _city = value;
   }
 
-
   String get description => _description;
 
   set description(String value) {
@@ -38,54 +55,50 @@ class CurrentWeather {
     _description = value;
   }
 
-
   double get currentTemp => _currentTemp;
 
-  set currentTemp(double value){
-    if(value > 100 || value < -100){
+  set currentTemp(double value) {
+    if (value > 100 || value < -100) {
       throw Exception('Temperature must be between -100 and 100');
     }
 
     _currentTemp = value;
   }
 
-
   DateTime get currentTime => _currentTime;
 
-  set currentTime(DateTime value){
-    if (value.isAfter(DateTime.now())){
+  set currentTime(DateTime value) {
+    if (value.isAfter(DateTime.now())) {
       throw Exception('Current time cannot be in the future');
     }
 
     _currentTime = value;
   }
 
-
   DateTime get sunrise => _sunrise;
 
-  set sunrise(DateTime value){
-    if (!(value.year == currentTime.year &&
-        value.month == currentTime.month &&
-        value.day == currentTime.day)){
-      throw Exception('Sunrise must be on the same day as current time');
-    } else if (value.isAfter(sunset)){
-      throw Exception('Sunrise cannot be after sunset');
-    }
+  set sunrise(DateTime value) {
+    // if (!(value.year == currentTime.year &&
+    //     value.month == currentTime.month &&
+    //     value.day == currentTime.day)) {
+    //   throw Exception('Sunrise must be on the same day as current time');
+    // } else if (value.isAfter(sunset)) {
+    //   throw Exception('Sunrise cannot be after sunset');
+    // }
 
     _sunrise = value;
   }
 
-
   DateTime get sunset => _sunset;
 
-  set sunset(DateTime value){
-    if (!(value.year == currentTime.year &&
-        value.month == currentTime.month &&
-        value.day == currentTime.day)){
-      throw Exception('Sunset must be on the same day as current time');
-    } else if (value.isBefore(sunrise)){
-      throw Exception('Sunset cannot be before sunrise');
-    }
+  set sunset(DateTime value) {
+    // if (!(value.year == currentTime.year &&
+    //     value.month == currentTime.month &&
+    //     value.day == currentTime.day)) {
+    //   throw Exception('Sunset must be on the same day as current time');
+    // } else if (value.isBefore(sunrise)) {
+    //   throw Exception('Sunset cannot be before sunrise');
+    // }
 
     _sunset = value;
   }
@@ -98,23 +111,20 @@ class CurrentWeather {
     final description = data['weather'][0]['description'];
     final currentTemp = data['main']['temp'].toDouble();
     final currentTime = DateTime.fromMillisecondsSinceEpoch(data['dt'] * 1000);
-    final sunrise = DateTime.fromMillisecondsSinceEpoch(data['sys']['sunrise'] * 1000);
-    final sunset = DateTime.fromMillisecondsSinceEpoch(data['sys']['sunset'] * 1000);
+    final sunrise =
+        DateTime.fromMillisecondsSinceEpoch(data['sys']['sunrise'] * 1000);
+    final sunset =
+        DateTime.fromMillisecondsSinceEpoch(data['sys']['sunset'] * 1000);
 
-    return CurrentWeather(city, description, currentTemp, currentTime, sunrise, sunset);
+    return CurrentWeather(
+        city, description, currentTemp, currentTime, sunrise, sunset);
   }
 
   @override
-  String toString() => "City: $city, Description: $description, Current Temperature: $currentTemp, Current Time: $currentTime, Sunrise: $sunrise, Sunset: $sunset";
+  String toString() =>
+      "City: $city, Description: $description, Current Temperature: $currentTemp, Current Time: $currentTime, Sunrise: $sunrise, Sunset: $sunset";
 
   //endregion
-
-
-
-
-
-
-
 
 
 }
